cc32e42828ac1345d832da203540c422a3716208
Tom Garcia <tomgarcia85@gmail.com>
2018-06-14 14:31:35 -0500

    first project with VS

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 13;
+            int num2 = 14;
+            int sum = num1 + num2;
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is" + sum);
+            Console.Read();
+
+        }
+    }
+}
