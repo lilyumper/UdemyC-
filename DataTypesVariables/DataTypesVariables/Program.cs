b5de194c107058bd82c1a4c8d6e2f4bf61046ba4
Tom Garcia <tomgarcia85@gmail.com>
2018-07-04 13:23:00 -0500

    Datatypes for c#

---
+++ DataTypesVariables/DataTypesVariables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DataTypesVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Mystring = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(Mystring);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
+            string result = Mystring + mySecondString;
+
+            Console.WriteLine(resultInt);
+            Console.Read();
+        }
+    }
+}
---
+++ DatatypesChallenge/DatatypesChallenge/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DatatypesChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte
+
+            byte thisbyte = 8;
+
+            //sbyte 
+            sbyte anotherByte = -33;
+
+            //int
+            int num1 = 23;
+
+            //uint
+
+            uint unassignedNum = 2;
+
+            //short
+
+            short shortie = 23400;
+
+            //ushort ... unassignedshort cant be negative
+
+            ushort uShortie = 55;
+
+            //long
+
+            long longJohn = 555555;
+
+            //ulong... cant be negative
+
+            ulong Silver = 234342423;
+
+            //float
+            float myFloat = 3.14F;
+
+            //double
+
+            double myDouble = 1.555555;
+
+            //char
+
+            char myChar = 't';
+
+            //string
+
+            string myString = "I control text";
+
+            string anotherString = "13";
+
+            //parse
+            int parsed = Int32.Parse(anotherString);
+
+
+            Console.WriteLine(parsed);
+
+        }
+    }
+}
