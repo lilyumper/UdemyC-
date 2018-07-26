f01c56c639ad65a36384f93752c1c4be4ead1b79
Tom Garcia <tomgarcia85@gmail.com>
2018-07-26 15:17:18 -0500

    Nested ifs and Ors

---
+++ NestedIf/NestedIf/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NestedIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Please enter your username");
+            userName = Console.ReadLine();
+
+            if (isRegistered && userName != "" && userName.Equals("admin"))
+            {
+                Console.WriteLine("Hi there, registered user");
+
+                Console.WriteLine("Hi There, " + userName);
+
+                Console.WriteLine("Hi there, Admin");
+
+            }
+
+            if(isAdmin || isRegistered){
+                Console.WriteLine("You are logged in");
+            }
+            Console.Read();
+        }
+
+
+    }
+}
+
