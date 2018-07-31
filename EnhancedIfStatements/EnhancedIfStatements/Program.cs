29de6de71498eb7c73086d1b79b9a15d078e95c7
Tom Garcia <tomgarcia85@gmail.com>
2018-07-31 17:10:30 -0500

    Enhanced If Statements

---
+++ EnhancedIfStatements/EnhancedIfStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EnhancedIfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = -5;
+            string stateOfMatter;
+
+
+            if(temperature <0){
+                stateOfMatter = "solid";
+            }
+            else{
+                stateOfMatter = "liquid";
+            }
+            //in short
+            temperature += 30;
+            Console.WriteLine("State of Matter is {0}", stateOfMatter);
+
+            temperature += 100;
+            stateOfMatter = temperature >100 ? "gas" : (temperature < 0 ? "solid" : "liquid");
+            Console.WriteLine("State of Matter is {0}", stateOfMatter);
+
+        }
+    }
+}
