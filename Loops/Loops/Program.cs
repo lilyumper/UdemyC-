afc215860d226d207e06c428ce59a14f8a43aedd
Tom Garcia <tomgarcia85@gmail.com>
2018-08-06 18:24:01 -0500

    For Loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int counter = 0; counter < 20; counter++){
+                if(counter % 2 == 1){
+                    
+                    Console.WriteLine(counter);
+                }
+
+            }
+            Console.WriteLine("the Loop is done!");
+            Console.Read();
+        }
+    }
+}
