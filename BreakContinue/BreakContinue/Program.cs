e14e491a6433f32d63fbe06a01e4032dee155cff
Tom Garcia <tomgarcia85@gmail.com>
2018-08-10 13:15:58 -0500

    Break and Continue Examples

---
+++ BreakContinue/BreakContinue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int counter = 0; counter < 10; counter++){
+                if(counter %2 == 0){
+                    Console.WriteLine("Now comes an odd number");
+                    continue;
+                }
+                Console.WriteLine(counter);
+            }
+            
+        }
+    }
+}
