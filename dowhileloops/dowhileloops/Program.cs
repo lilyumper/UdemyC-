65216a26ebbdd0e102d57d87187adabcc4dc1167
Tom Garcia <tomgarcia85@gmail.com>
2018-08-10 12:16:45 -0500

    Do While

---
+++ dowhileloops/dowhileloops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace dowhileloops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wholeText = "";
+            int lengthOfText = 0;
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfAFriend = Console.ReadLine();
+                int currentLength = nameOfAFriend.Length;
+                lengthOfText += currentLength;
+                wholeText += " " + nameOfAFriend;
+            
+
+            }
+            while (lengthOfText < 20);
+            Console.WriteLine("Thanks, that was enough " + "  "+ wholeText);
+
+            Console.Read();
+        }
+    }
+}
