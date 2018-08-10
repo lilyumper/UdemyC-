5c2b979209046803ff5d705634cbd9ae8f2b24ce
Tom Garcia <tomgarcia85@gmail.com>
2018-08-10 12:44:23 -0500

    While Loops

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int peoplecounter = 0;
+            string entry = "";
+
+            while (entry.Equals(""))
+            {
+                Console.WriteLine("PLease press enter while blank to count students");
+                entry = Console.ReadLine();
+                peoplecounter++;
+                Console.WriteLine("Current student count is {0}", peoplecounter);
+            }
+            Console.WriteLine("{0} students are inside the bus. Please press enter to end program", peoplecounter);
+        }
+    }
+
+}
