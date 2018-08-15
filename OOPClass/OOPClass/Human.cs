7ded68d15f3f7014bfcf6b8a08374870efb63478
Tom Garcia <tomgarcia85@gmail.com>
2018-08-15 12:43:42 -0500

    Classes without constructors

---
+++ OOPClass/OOPClass/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace OOPClass
+{
+    //blueprint for a datatype
+    public class Human
+    {
+
+            //member variable
+            public string firstName;
+            public string lastName;
+
+            //member method
+            public void IntroduceMyself()
+            {
+            Console.WriteLine("Hi, im {0} {1}", firstName, lastName);
+            }
+
+
+        }
+    }
+
---
+++ OOPClass/OOPClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOPClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an objec of my class
+            // an instance of human
+            Human denis = new Human();
+            //access public variable from outside, and even change it
+            denis.firstName = "Denis";
+            //call mehthods of the class
+            denis.IntroduceMyself();
+            Human Tom = new Human();
+            Tom.firstName = "Tom";
+            Tom.IntroduceMyself();
+
+            //Challenge
+            Human Vinny = new Human();
+            Vinny.firstName = "Vinny";
+            Vinny.lastName = "Garcia";
+            Vinny.IntroduceMyself();
+
+            Human Jenny = new Human();
+            Jenny.firstName = "Jenny";
+            Jenny.lastName = "Garcia";
+            Jenny.IntroduceMyself();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
